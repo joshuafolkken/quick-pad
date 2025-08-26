a7a617dcbed6b1c801e398ff31535442a12a3033
joshuafolkken <joshuafolkken@gmail.com>
2025-08-26 11:03:33 +0900

    shortcuts #20

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,6 +3,21 @@ extends Node2D
 const NODE_PATH_FORMAT = "CanvasLayer/VBoxContainer/Row%d/Pad%d"
 const FILE_PATH_FORMAT = "res://assets/audio/%s"
 
+const SHORTCUT_MAPPING: Dictionary = {
+	KEY_2: [0, 0],
+	KEY_3: [0, 1],
+	KEY_4: [0, 2],
+	KEY_Q: [1, 0],
+	KEY_W: [1, 1],
+	KEY_E: [1, 2],
+	KEY_A: [2, 0],
+	KEY_S: [2, 1],
+	KEY_D: [2, 2],
+	KEY_Z: [3, 0],
+	KEY_X: [3, 1],
+	KEY_C: [3, 2],
+}
+
 const FILE_NAMES: Array = [
 	["和太鼓でドン.mp3", "和太鼓でドドン.mp3", "チーン1.mp3"],
 	["歓声と拍手1.mp3", "ラッパのファンファーレ.mp3", "ドンドンパフパフ.mp3"],
@@ -27,3 +42,22 @@ func _ready() -> void:
 			var pad: Pad = get_node(node_path)
 			var file_path := FILE_PATH_FORMAT % file_name
 			pad._load_audio_file(file_path)
+
+
+func _input(event: InputEvent) -> void:
+	if event is not InputEventKey:
+		return
+
+	var event_key: InputEventKey = event
+
+	if event_key.pressed and not event_key.echo:
+		if not SHORTCUT_MAPPING.has(event_key.keycode):
+			return
+
+		var coords: Array = SHORTCUT_MAPPING[event_key.keycode]
+		var row: int = coords[0]
+		var column: int = coords[1]
+
+		var node_path := NODE_PATH_FORMAT % [row, column]
+		var pad: Pad = get_node(node_path)
+		pad.play_audio()

19c6c3cdd2832b81bcc3c284620dbbbf390efece
joshuafolkken <joshuafolkken@gmail.com>
2025-08-26 11:21:21 +0900

    load audio on web #23

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,6 +3,21 @@ extends Node2D
 const NODE_PATH_FORMAT = "CanvasLayer/VBoxContainer/Row%d/Pad%d"
 const FILE_PATH_FORMAT = "res://assets/audio/%s"
 
+const AUDIO_UID_MAPPING: Dictionary[String, String] = {
+	"和太鼓でドン.mp3": "uid://ddd8foqjrnovn",
+	"和太鼓でドドン.mp3": "uid://bw52nji8ay54d",
+	"チーン1.mp3": "uid://wbv01t4b6ymq",
+	"歓声と拍手1.mp3": "uid://bx81nwfhegkd2",
+	"ラッパのファンファーレ.mp3": "uid://can1jh5nm483x",
+	"ドンドンパフパフ.mp3": "uid://bw52nji8ay54d",
+	"ツッコミを入れる.mp3": "uid://c742twl6jo8l",
+	"「わぁーーっ♪」.mp3": "uid://cbofi5pxhwb3e",
+	"自主規制ピー音.mp3": "uid://tew6dwyl5fcn",
+	"クイズ出題1.mp3": "uid://beelu7mlxxqdy",
+	"クイズ正解1.mp3": "uid://di3uii830ctpo",
+	"クイズ不正解1.mp3": "uid://daqt5a22itqmr",
+}
+
 const SHORTCUT_MAPPING: Dictionary = {
 	KEY_2: [0, 0],
 	KEY_3: [0, 1],
@@ -40,8 +55,12 @@ func _ready() -> void:
 
 			var node_path := NODE_PATH_FORMAT % [row, column]
 			var pad: Pad = get_node(node_path)
-			var file_path := FILE_PATH_FORMAT % file_name
-			pad._load_audio_file(file_path)
+
+			if not AUDIO_UID_MAPPING.has(file_name):
+				return
+
+			var uid := AUDIO_UID_MAPPING[file_name]
+			pad.load_audio_by_uid(uid, file_name)
 
 
 func _input(event: InputEvent) -> void:
